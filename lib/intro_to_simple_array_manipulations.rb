0931997fd826799c70ac3056f2d787904d3015fc
Andy Choi <wooseokchoi96@gmail.com>
2019-07-11 19:49:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,9 +11,7 @@ def using_pop(array)
 end 
 
 def pop_with_args(array)
-  2.times do
-    array.pop
-  end
+    array.pop(2)
 end
 
 def using_shift(array)
@@ -21,9 +19,7 @@ def using_shift(array)
 end
 
 def shift_with_args(array)
-  2.times do
-    array.shift 
-  end
+    array.shift(2)
 end
 
 def using_concat(first,second)
